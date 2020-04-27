dae4db8c0d59ca374fa23b2f80ce788c19101f70
Mon-Lay <49906170+Mon-Lay@users.noreply.github.com>
2020-04-27 23:30:13 +0800

    Add Class for Bonus Question

---
+++ Bonus_Solution.java
@@ -0,0 +1,10 @@
+package com.my.assignment;
+
+public class Bonus_Solution {
+
+	/*
+	   1 answer is Myanmar : aww-e-e-ouk 
+	   
+	   2 : I didn't designed because I haven't time and don't understand surely.
+	*/ 
+}
